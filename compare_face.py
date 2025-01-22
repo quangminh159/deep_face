8f3f3f2248d4358475740b3297d60a1b2c80e15e
quangminh159 <quangminh159159@gmail.com>
2025-01-22 14:51:54 +0700

    Mô tả thay đổi của bạn

--- compare_face.py
+++ compare_face.py
@@ -1,6 +1,7 @@
 from deepface import DeepFace
 import numpy as np
 import cv2
+from mtcnn import MTCNN
 def cosine_similarity(embedding1, embedding2):
     e1 = np.array(embedding1)
     e2 = np.array(embedding2)
@@ -9,15 +10,17 @@ def get_embedding(image_path, model_name="Facenet512"):
     embedding = DeepFace.represent(img_path=image_path, model_name=model_name, enforce_detection=False)[0]["embedding"]
     return embedding
 def detect_and_crop_face(image_path):
+    # Đọc ảnh
     image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    detector = MTCNN()
+    results = detector.detect_faces(rgb_image)
 
-    if len(faces) > 0:
-        (x, y, w, h) = faces[0]
-        face = image[y:y+h, x:x+w]
-        return face, (x, y, w, h)
+    if results:
+        x, y, width, height = results[0]['box']
+        x, y = max(0, x), max(0, y)  
+        face = image[y:y + height, x:x + width]
+        return face, (x, y, width, height)
     return None, None
 def compare_images(image_path1, image_path2, model_name="Facenet512", threshold=0.5):
     embedding1 = DeepFace.represent(img_path=image_path1, model_name=model_name, enforce_detection=False)[0]["embedding"]
